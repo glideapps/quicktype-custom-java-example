8315b3e23747d3e7f3442a8c853962ccdd2ec71e
Mark Probst <mark.probst@gmail.com>
2018-02-06 08:20:10 -0800

    Show more customization

--- app.ts
+++ app.ts
@@ -6,8 +6,9 @@ import { Run, Options } from "quicktype";
 import { JavaTargetLanguage, JavaRenderer } from "quicktype/dist/Language/Java"
 import { TypeGraph } from "quicktype/dist/TypeGraph";
 import { ConvenienceRenderer } from "quicktype/dist/ConvenienceRenderer";
-import { ClassType, ClassProperty } from "quicktype/dist/Type";
+import { ClassType, ClassProperty, Type } from "quicktype/dist/Type";
 import { Name, FixedName } from "quicktype/dist/Naming";
+import { capitalize } from "quicktype/dist/Strings";
 
 class CustomJavaTargetLanguage extends JavaTargetLanguage {
     protected get rendererClass(): new (
@@ -20,11 +21,22 @@ class CustomJavaTargetLanguage extends JavaTargetLanguage {
 }
 
 class CustomJavaRenderer extends JavaRenderer {
-    protected nameForProperty(_c: ClassType, _className: Name, _p: ClassProperty, jsonName: string): Name {
+    protected makeNameForNamedTyped(t: Type): Name {
+        return new FixedName(`${capitalize(t.kind)}_${t.getCombinedName()}`);
+    }
+
+    protected makeNameForTopLevel(t: Type, givenName: string, maybeNamedType: Type | undefined): Name {
+        if (maybeNamedType !== undefined) {
+            return this.makeNameForNamedTyped(maybeNamedType);
+        }
+        return new FixedName(`${capitalize(t.kind)}_${givenName}`);
+    }
+
+    protected makeNameForProperty(_c: ClassType, _className: Name, _p: ClassProperty, jsonName: string): Name {
         return new FixedName(jsonName);
     }
 
-    protected getterAndSetterNamesForProperty(
+    protected makeNamesForPropertyGetterAndSetter(
         _c: ClassType,
         _className: Name,
         _p: ClassProperty,
